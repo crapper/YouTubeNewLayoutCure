c598aacac43a816643b47c1644e6ec18b9aed816
Fakeye <crapper.612@gmail.com>
2024-04-14 18:51:59 +0800

    Update inject.js

--- public/inject.js
+++ public/inject.js
@@ -4,7 +4,7 @@ const timerId = setInterval(() => {
   if (testingElement === null) return;
 
   document.querySelector("#columns").style = "flex-direction: row-reverse"; 
-  document.querySelector("#primary").style = "min-width: auto";
-  document.querySelector("#secondary").style = "flex: 1 1 auto; width: 80%;";
+  document.querySelector("#primary").style = "min-width: auto; width: 20vw;";
+  document.querySelector("#secondary").style = "flex: 1 1 auto; width: 80vw;";
   // clearInterval(timerId);
 }, 1000);
