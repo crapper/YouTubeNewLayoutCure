72c9286a06c40e618d0ca846ba30de5f52026f0c
Fakeye <crapper.612@gmail.com>
2024-04-14 22:32:27 +0800

    Update manifest.json and inject.js

--- public/inject.js
+++ public/inject.js
@@ -2,9 +2,14 @@
 const timerId = setInterval(() => {
   const testingElement = document.querySelector("#columns > #primary");
   if (testingElement === null) return;
-
-  document.querySelector("#columns").style = "flex-direction: row-reverse"; 
-  document.querySelector("#primary").style = "min-width: auto; width: 20vw;";
-  document.querySelector("#secondary").style = "flex: 1 1 auto; width: 80vw;";
-  // clearInterval(timerId);
+  const url = window.location.href;
+  if (url.includes("watch")) {
+    console.log("Injecting CSS");
+    document.querySelector("#columns").style = "flex-direction: row-reverse"; 
+    document.querySelector("#primary").style = "min-width: auto; width: 20vw;";
+    document.querySelector("#secondary").style = "flex: 1 1 auto; width: 80vw;";
+    // clearInterval(timerId);
+  }else{
+    console.log("Not on watch page");
+  }
 }, 1000);
