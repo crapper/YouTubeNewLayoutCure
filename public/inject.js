e2700b1a7bb27a286ed3ec7d8c40949ae51b38b1
Fakeye <crapper.612@gmail.com>
2024-04-14 22:53:21 +0800

    Update inject.js

--- public/inject.js
+++ public/inject.js
@@ -1,15 +1,38 @@
 // YouTube new layout cure
 const timerId = setInterval(() => {
-  const testingElement = document.querySelector("#columns > #primary");
-  if (testingElement === null) return;
+  var columnOriginalStyle = "";
+  var primaryOriginalStyle = "";
+  var secondaryOriginalStyle = "";
+  const columnsElement = document.querySelector("#columns");
+  const primaryElement = document.querySelector("#columns > #primary");
+  const secondaryElement = document.querySelector("#columns > #secondary");
+  if (columnsElement === null || primaryElement === null || secondaryElement === null) return;
+
   const url = window.location.href;
   if (url.includes("watch")) {
     console.log("Injecting CSS");
     document.querySelector("#columns").style = "flex-direction: row-reverse"; 
-    document.querySelector("#primary").style = "min-width: auto; width: 20vw;";
-    document.querySelector("#secondary").style = "flex: 1 1 auto; width: 80vw;";
+    document.querySelector("#columns > #primary").style = "min-width: auto; width: 20vw;";
+    document.querySelector("#columns > #secondary").style = "flex: 1 1 auto; width: 80vw;";
     // clearInterval(timerId);
   }else{
-    console.log("Not on watch page");
+    console.log("Change the style back to original");
+    if (columnOriginalStyle === "" || primaryOriginalStyle === "" || secondaryOriginalStyle === "") return;
+    document.querySelector("#columns").style = columnOriginalStyle;
+    document.querySelector("#primary").style = primaryOriginalStyle;
+    document.querySelector("#secondary").style = secondaryOriginalStyle;
+  }
+
+  const columnStyle = columnsElement.getAttribute("style");
+  const primaryStyle = primaryElement.getAttribute("style");
+  const secondaryStyle = secondaryElement.getAttribute("style");
+  if (columnStyle === "flex-direction: row-reverse;" && primaryStyle === "min-width: auto; width: 20vw;" && secondaryStyle === "flex: 1 1 auto; width: 80vw;") {
+    console.log("Already injected CSS");
+    return;
+  }else{
+    // keep the original style, for home page
+    columnOriginalStyle = columnStyle;
+    primaryOriginalStyle = primaryStyle;
+    secondaryOriginalStyle = secondaryStyle;
   }
 }, 1000);
